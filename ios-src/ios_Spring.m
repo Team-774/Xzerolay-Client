6bf3e10aba869e5fce7c6d7135ee0eed4c787504
Linus Järvinen <44102444+LJarvinen@users.noreply.github.com>
2018-10-12 22:51:34 -1000

    Update ios_Spring.m

--- ios-src/ios_Spring.m
+++ ios-src/ios_Spring.m
@@ -3,7 +3,7 @@ import <Foundation/Foundation.h>
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World!");   
     }
     return 0;
 }
