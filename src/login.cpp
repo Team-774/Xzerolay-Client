17a651981dc91da91b7f5e31d11a642149791a4f
Linus Järvinen <44102444+LJarvinen@users.noreply.github.com>
2018-10-12 22:33:37 -1000

    Update and rename classObj-1.cpp to login.cpp

---
+++ src/login.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <Windows.h>
+
+#include "pch.h"
+#include "process.cpp"
+
+using namespace std;
+
+void getLogin() {
+  // Default Placeholders! Change later
+  string username[] = {"admin", "guest"};
+  string password[] = "XZ17612109";
+  string userInput;
+  string passInput;
+  int guessCount = 0;
+  int guessLimit = 3;
+  bool outOfGuesses = false;
+  
+  while (username != userInput && !oufOfGuesses) {
+    if (guessCount < guessLimit) {
+      cout << "Username:";
+      getline(cin, userInput);
+      guessCount++;
+    } else {
+      outOfGuesses = true;
+    }
+}
+  
+  if (outOfGuesses) {
+    system("color 4");
+    cout << "LOGIN_ERROR! Failed to login after 3 failed attempts!" << endl;
+    return 0;
+} else {
+    system("color a");
+    cout << "Success! Logged into username. Checking password... << endl;
+    Sleep(6000);
+}
+  
+  guessCount = 0;
+  
+  cout << "Welcome, user!";
+  
+  while (password != passInput && !oufOfGuesses) {
+    if (guessCount < guessLimit) {
+      cout << "Password:";
+      getline(cin, passInput);
+      guessCount++;
+    } else {
+      outOfGuesses = true;
+    }
+}
+  
+    if (outOfGuesses) {
+    system("color 4");
+    cout << "LOGIN_ERROR! Failed to login after 3 failed attempts!" << endl;
+    return 0;
+  } else {
+       system("color a");
+       cout << "Success! Going through process finder..." << endl
+  }
+  
+  return 1;
+}
+
+int main() {
+  
+  getLogin();
+  
+  if (!getLogin()) {
+    system("color 4");
+    cout << "FATAL_ERROR! Failed to login!" << endl;
+    Sleep(1000);
+    return 0;
+  } else {
+    system("color e");
+    cout << "Success! Logging into system..." << endl;
+    Sleep(1000);
+  }
+  
+  return 0; 
+}
